18f52c31552fe3b5366ee2f08ea018af5f00ccc1
João Pedro Barbosa <info.joaopedrobarbosa@gmail.com>
2024-11-19 19:21:25 -0300

    feat: appbar

--- avaliacao_1/lib/src/diario/presentation/diario.dart
+++ avaliacao_1/lib/src/diario/presentation/diario.dart
@@ -9,6 +9,28 @@ class Diario extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 70,
+          centerTitle: true,
+          leading: IconButton(
+              padding: const EdgeInsets.only(left: 20),
+              onPressed: () {}, 
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.white,
+                size: 40,
+              )
+            ),
+          title: const Text(
+            'Diário',
+            style: TextStyle(
+              color: Colors.white, 
+              fontSize: 30,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          backgroundColor: const Color.fromRGBO(36, 36, 75, 1),
+        ),
         body: Container(
           decoration: const BoxDecoration(
             color: Color.fromRGBO(101, 97, 142, 1),
@@ -17,7 +39,6 @@ class Diario extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             children: [
               TelaInicial(),
-              
             ],
           ),
         ),
